03316dc97f5ec43768b6736b8ce0df1d247cc3e2
Joseph Finney <josephfinney@LIVE.COM>
2025-05-05 21:41:22 -0500

    Use context of the packing agent

--- SimplePackingList/Services/WcrService.cs
+++ SimplePackingList/Services/WcrService.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Windows.AI.Generative;
+﻿using Microsoft.Windows.AI.ContentModeration;
+using Microsoft.Windows.AI.Generative;
 using System;
 using System.Threading.Tasks;
+using Windows.Foundation;
 
 namespace SimplePackingList.Services;
 
@@ -8,17 +10,26 @@ public class WcrService
 {
     public async Task<string> TextResponseWithProgress(string prompt, IProgress<string> progress)
     {
-        using LanguageModel languageModel = await LanguageModel.CreateAsync();
-        TextRewriter textSummarizer = new(languageModel);
+        string baseContext = """
+            You are a helpful assistant. You will be provided with a recent weather report. Do not mention the date or the weather report. You will help the user by describing the best kinds clothes to wear given the weather. Do not include any other information or context. Keep responses short. Start each line with hyphen and put each recommendation on its own line.
+            """;
 
-        Windows.Foundation.IAsyncOperationWithProgress<LanguageModelResponseResult, string> textSummarizerResponseWithProgress = textSummarizer.RewriteAsync(prompt);
+        using LanguageModel languageModel = await LanguageModel.CreateAsync();
+        LanguageModelOptions languageModelOptions = new()
+        {
+            Temperature = 0.5f
+        };
+
+        LanguageModelContext contextModel = languageModel.CreateContext(baseContext);
+
+        IAsyncOperationWithProgress<LanguageModelResponseResult, string> responseAndProgress = languageModel.GenerateResponseAsync(contextModel, prompt, languageModelOptions);
 
         string progressText = string.Empty;
-        textSummarizerResponseWithProgress.Progress = (_, generationProgress) =>
+        responseAndProgress.Progress = (_, generationProgress) =>
         {
             progress.Report(generationProgress);
         };
-        LanguageModelResponseResult response = await textSummarizerResponseWithProgress;
+        LanguageModelResponseResult response = await responseAndProgress;
         return response.Text;
     }
 }
