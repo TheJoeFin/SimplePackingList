a874dc9266c64ad76c7857b8f70eccb904b0d73e
Joseph Finney <josephfinney@live.com>
2025-05-22 12:05:52 -0500

    New trip class and local settings service

---
+++ SimplePackingList/Models/Trip.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
+using System;
+
+namespace SimplePackingList.Models;
+
+public partial class Trip : ObservableObject
+{
+    [ObservableProperty]
+    public partial string Id { get; set; } = string.Empty;
+
+    [ObservableProperty]
+    public partial string Name { get; set; } = string.Empty;
+
+    [ObservableProperty]
+    public partial string PlaceEntry { get; set; } = string.Empty;
+
+    [ObservableProperty]
+    public partial PlacePrediction? Place { get; set; }
+
+    [ObservableProperty]
+    public partial DateTimeOffset? StartDate { get; set; }
+
+    [ObservableProperty]
+    public partial DateTimeOffset? EndDate { get; set; }
+
+    [ObservableProperty]
+    public partial OpenWeatherResponse? OpenWeatherResponse { get; set; }
+
+    [ObservableProperty]
+    public partial DateTimeOffset GotForecastOn { get; set; }
+
+    [ObservableProperty]
+    public partial string PackingText { get; set; } = string.Empty;
+
+    public Trip()
+    {
+        
+    }
+
+    public static Trip NewTrip()
+    {
+        Trip trip = new()
+        {
+            Id = Guid.NewGuid().ToString()
+        };
+        return trip;
+    }
+}
---
+++ SimplePackingList/Services/LocalSettingsService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.Json;
+using Windows.Storage;
+
+namespace SimplePackingList.Services;
+
+public static class LocalSettingsService
+{
+    static public T? ReadSettingAsync<T>(string key)
+    {
+        try
+        {
+            if (ApplicationData.Current.LocalSettings.Values.TryGetValue(key, out object? obj))
+            {
+                return JsonSerializer.Deserialize<T>((string)obj);
+            }
+        }
+        catch (Exception)
+        {
+            Debug.WriteLine($"Failed to read setting: {key}");
+        }
+        return default;
+
+    }
+
+    static public void SaveSettingAsync<T>(string key, T value)
+    {
+        try
+        {
+            ApplicationData.Current.LocalSettings.Values[key] = JsonSerializer.Serialize(value);
+        }
+        catch (Exception)
+        {
+            Debug.WriteLine($"Failed to write setting: {key}\t{value?.ToString()}");
+        }
+    }
+}
+
+public enum SettingKeys
+{
+    LastTrip,
+    PastTrips,
+}
